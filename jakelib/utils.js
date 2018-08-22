0a19c44b6400b9a4b14b2daaac13e333d365a230
David Clark <dclark@greenchef.com>
2018-08-22 11:17:25 -0600

    Refactored serviceToPath and getBranchName into utils file.

---
+++ jakelib/utils.js
@@ -0,0 +1,32 @@
+const gitBranch = require('git-branch')
+
+const { PATH_TO_SERVER, PATH_TO_CONSOLE, PATH_TO_CONSUMER, PATH_TO_SHIPPING, PATH_TO_INVENTORY } = process.env;
+
+const serviceToPath = (service_name) => {
+  switch(service_name) {
+    case 'consoleapi':
+    case 'web-api':
+      return PATH_TO_SERVER
+    case 'console':
+      return PATH_TO_CONSOLE
+    case 'consumer':
+      return PATH_TO_CONSUMER
+    case 'shipping-api':
+    case 'shipping-worker':
+      return PATH_TO_SHIPPING
+    case 'inventory-worker':
+      return PATH_TO_INVENTORY
+    default:
+      return null;
+  }
+}
+
+const getBranchName = (service_name) => {
+  const path = serviceToPath(service_name);
+  if (!path) return 'unknown'
+
+  return gitBranch.sync(path);
+}
+
+exports.serviceToPath = serviceToPath;
+exports.getBranchName = getBranchName;
