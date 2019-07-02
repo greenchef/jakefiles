247d1022ed5003c5c608cce9bae0bb87a7e8161c
David Clark <dclark@greenchef.com>
2019-07-02 12:22:56 -0600

    Adds support for bifrost deployments and DRYing up the code.

--- jakelib/utils.js
+++ jakelib/utils.js
@@ -3,40 +3,43 @@ const simpleGit = require('simple-git/promise')
 const {
   GITHUB_USERNAME,
   PATH_TO_ANALYTICS,
+  PATH_TO_BIFROST,
   PATH_TO_CONSOLE,
   PATH_TO_CONSOLE_V2,
   PATH_TO_CONSUMER,
   PATH_TO_INVENTORY,
+  PATH_TO_JSREPORTS,
   PATH_TO_SERVER,
   PATH_TO_SHIPPING,
-  PATH_TO_JSREPORTS
 } = process.env;
 
 const serviceToPath = (service_name) => {
   switch(service_name) {
-    case 'consoleapi':
-    case 'web-api':
-    case 'worker':
-    case 'scheduler':
-      return PATH_TO_SERVER
+    case 'analytics-mosql-models':
+    case 'analytics-mosql-shipping':
+    case 'analytics-mosql-logevents':
+      return PATH_TO_ANALYTICS
+    case 'bifrost':
+      return PATH_TO_BIFROST
     case 'console':
       return PATH_TO_CONSOLE
     case 'console-v2':
       return PATH_TO_CONSOLE_V2
     case 'consumer':
       return PATH_TO_CONSUMER
+    case 'jsreports':
+      return PATH_TO_JSREPORTS
+    case 'inventory-worker':
+      return PATH_TO_INVENTORY
+    case 'consoleapi':
+    case 'web-api':
+    case 'worker':
+    case 'scheduler':
+      return PATH_TO_SERVER
     case 'shipping-api':
     case 'shipping-worker':
     case 'shipping-scheduler':
       return PATH_TO_SHIPPING
-    case 'inventory-worker':
-      return PATH_TO_INVENTORY
-    case 'analytics-mosql-models':
-    case 'analytics-mosql-shipping':
-    case 'analytics-mosql-logevents':
-      return PATH_TO_ANALYTICS
-    case 'jsreports':
-      return PATH_TO_JSREPORTS
     default:
       throw new Error(`Unable to find path for service name: ${service_name}`);
   }
